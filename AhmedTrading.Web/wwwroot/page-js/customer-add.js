91596ea394a87a121bea17ad39753aeca4cbdd7e
muhebollah <muhebollah.diu@gamil.com>
2020-07-15 20:36:27 +0600

    colors change

---
+++ AhmedTrading.Web/wwwroot/page-js/customer-add.js
@@ -0,0 +1,120 @@
+﻿
+//show hide field
+function radioSelected(evt) {
+    const individual = document.querySelectorAll('.individual-field')
+    const corporate = document.querySelectorAll('.corporate-field')
+
+    if (evt.value === 'true') {
+        individual.forEach(item => {
+            item.style.display = 'block';
+        })
+
+        corporate.forEach(item => {
+            item.style.display = 'none';
+        })
+    } else {
+        individual.forEach(item => {
+            item.style.display = 'none';
+        })
+
+        corporate.forEach(item => {
+            item.style.display = 'block';
+        })
+    }
+};
+
+
+//selectors
+const phoneContainer = document.getElementById("phone-wrapper");
+const customerForm = document.getElementById("customer-form");
+
+//functions
+let elementIndex = 1;
+const isError = [];
+
+const btnEnabledDisable = function () {
+    btnSubmit.disabled = isError.length ? true : false;
+}
+
+const checkPhoneIsExists = function (evt) {    
+    const phoneInput = evt.target.classList.contains("valid-check");
+    if (!phoneInput) return;
+
+    const mobile = evt.target.value;
+    const errorElement = evt.target.nextElementSibling;
+
+    if (errorElement.nodeName === "SPAN")
+        errorElement.remove();
+
+    if (mobile.length > 10) {
+        const url = '/Customer/CheckMobileIsAvailable';
+        const parameter = { params: { mobile } };
+        const request = axios.get(url, parameter);
+        const element = `<span class="field-validation-error">This Mobile Number Already Exists!</span>`;
+        const self = evt.target;
+        const errorIndex = isError.indexOf(self.id);
+
+        request.then(response => {
+            if (response.data) {
+                self.insertAdjacentHTML('afterend', element);
+                if (errorIndex === -1) isError.push(self.id);
+            }
+            else {
+                if (errorIndex !== -1) isError.splice(errorIndex, 1);
+            }
+
+            btnEnabledDisable();
+        }).catch(err => console.log(err));
+    }
+}
+
+const addInputElement = function () {
+    const element = `<div class="phone-container">
+                <div class="md-form m-0 flex-grow-1">
+                    <input id="phone-${elementIndex}" name="PhoneNumbers[${elementIndex}].Phone" required type="number" class="form-control valid-check" />
+                    <label for="phone-${elementIndex}">Phone Number</label>
+                </div>
+                <div class="add-element">
+                    <a class="btn-floating btn-sm bg-danger remove m-0"><i class="fas fa-minus"></i></a>
+                </div>
+            </div>`;
+
+    elementIndex++;
+    phoneContainer.insertAdjacentHTML('beforeend', element);
+}
+
+const removeInputElement = function (evt) {
+    evt.target.parentElement.parentElement.remove();
+    const id = evt.target.parentElement.previousElementSibling.children[0].id;
+    const errorIndex = isError.indexOf(id);
+
+    if (errorIndex !== -1) isError.splice(errorIndex, 1);
+
+    btnEnabledDisable();
+}
+
+const togglePhoneElement = function (evt) {
+    const addClicked = evt.target.classList.contains("add");
+    const removeClicked = evt.target.classList.contains("remove");
+
+    if (addClicked)
+        addInputElement(evt);
+
+    if (removeClicked)
+        removeInputElement(evt);
+}
+
+const onFormSubmit = function (evt) {
+    evt.target.btnSubmit.disabled = true;
+    evt.target.btnSubmit.innerText = "Please wait...";
+
+    setTimeout(()=> {
+        evt.target.btnSubmit.disabled = false;
+        evt.target.btnSubmit.innerText = "Add Customer";
+    }, 3000);
+}
+
+//events
+phoneContainer.addEventListener("click", togglePhoneElement);
+phoneContainer.addEventListener("input", checkPhoneIsExists);
+customerForm.addEventListener('submit', onFormSubmit);
---
+++ AhmedTrading.Web/wwwroot/page-js/expense.js
@@ -0,0 +1,113 @@
+﻿
+//selectors
+const tableBody = document.getElementById('table-body');
+const btnCreate = document.getElementById('CreateClick');
+const insertModal = $('#InsertModal');
+
+
+//functions
+const createLink = function (item) {
+    const link = document.createElement("a");
+
+    link.href = `/Expenses/Delete/${item.ExpenseId}`;
+    link.classList.add('delete', 'fas', 'fa-trash-alt');
+
+    return link;
+}
+
+const createTableRow = function (item) {
+    const tr = document.createElement("tr");
+
+    //column 1
+    const td1 = tr.insertCell(0);
+    const textNode1 = document.createTextNode(item.ExpenseAmount);
+    td1.appendChild(textNode1);
+    //column 2
+    const td2 = tr.insertCell(1);
+    const textNode2 = document.createTextNode(item.ExpenseFor? item.ExpenseFor: '');
+    td2.appendChild(textNode2);
+    //column 3
+    const td3 = tr.insertCell(2);
+    const textNode3 = document.createTextNode(item.ExpensePaymentMethod);
+    td3.appendChild(textNode3);
+    //column 4
+    const td4 = tr.insertCell(3);
+    const textNode4 = document.createTextNode(moment(item.ExpenseDate).format('DD MMM YYYY'));
+    td4.appendChild(textNode4);
+    //column 5
+    const td5 = tr.insertCell(4);
+    td5.appendChild(createLink(item));
+
+    return tr;
+}
+
+const displayExpense = function (data) {
+    console.log(data)
+    if (!data.length)
+        tableBody.innerHTML = "<tr><td colspan='5'>No record found!</td></tr>";
+    else
+        tableBody.innerHTML = '';
+
+    const fragment = document.createDocumentFragment();
+
+    data.forEach(item => {
+        const tr = createTableRow(item);
+        fragment.appendChild(tr);
+    });
+
+    tableBody.appendChild(fragment);
+}
+
+const getData = function () {
+    const url = '/Expenses/IndexData';
+    const request = axios.get(url);
+
+    request.then(response => displayExpense(response.data));
+}
+
+const onDeleteClicked = function (evt) {
+    evt.preventDefault();
+
+    const target = evt.target;
+    const deleteClicked = target.classList.contains('delete');
+
+    if (!deleteClicked) return;
+ 
+    const element = target.parentElement.parentElement;
+    const url = target.getAttribute("href");
+    if (!url) return;
+
+    const isConfirm = confirm("Are you sure you want to delete?");
+    if (!isConfirm) return;
+
+    axios.get(url).then(res => {
+        if (res.data === -1) {
+            target.removeAttribute("href");
+            element.insertAdjacentHTML('afterend', `<em class="used-error">"${element.innerText}" already used!</em>`);
+            return;
+        }
+        element.remove();
+    }).catch(err => console.log(err));
+}
+
+const onCreateClicked = function (evt) {
+    const url = evt.target.getAttribute("data-url");
+    axios.get(url).then(res => {
+        insertModal.html(res.data).modal('show');
+    });
+}
+
+function onCreateSuccess(data) {
+    if (data !== 'success') return;
+    insertModal.modal('hide');
+    getData();
+}
+
+
+
+//event listeners
+tableBody.addEventListener("click", onDeleteClicked);
+btnCreate.addEventListener('click', onCreateClicked);
+
+//call function
+getData();
