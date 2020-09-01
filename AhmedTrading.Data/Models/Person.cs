87d7c13378efe39f7c6145351eedf48248ff866b
Shahed Al Mahmud <shahedalmahmud@outlook.com>
2020-09-01 16:33:48 +0600

    Paid Multiple due

---
+++ AhmedTrading.Data/Models/Person.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace AhmedTrading.Data
+{
+    public class Person
+    {
+        public Person()
+        {
+            PersonalLoan = new HashSet<PersonalLoan>();
+        }
+        public int PersonId { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public virtual ICollection<PersonalLoan> PersonalLoan { get; set; }
+    }
+}
---
+++ AhmedTrading.Data/Models/PersonalLoan.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AhmedTrading.Data
+{
+    public class PersonalLoan
+    {
+        public int PersonalLoanId { get; set; }
+        public int PersonId { get; set; }
+        public int RegistrationId { get; set; }
+        public string LoanName { get; set; }
+        public double LoanAmount { get; set; }
+        public double ReturnAmount { get; set; }
+        public double RemainingAmount { get; set; }
+        public DateTime LoanDate { get; set; }
+        public DateTime InsertDate { get; set; }
+        public virtual Person Person { get; set; }
+        public virtual Registration Registration { get; set; }
+    }
+}
