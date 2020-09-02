9d9da9b0af194124017a372877f4471584f12529
muhebollah <muhebollah.diu@gamil.com>
2020-09-02 18:23:16 +0600

    loan okey

---
+++ AhmedTrading.Web/Controllers/PersonalLoanController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AhmedTrading.Repository;
+using JqueryDataTables.LoopsIT;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AhmedTrading.Web.Controllers
+{
+    [Authorize(Roles = "admin, personal-loan")]
+    public class PersonalLoanController : Controller
+    {
+        private readonly IUnitOfWork _db;
+        public PersonalLoanController(IUnitOfWork db)
+        {
+            _db = db;
+        }
+
+        //list of loan
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult IndexDataTable(DataRequest request)
+        {
+            var data = _db.Person.ListDataTable(request);
+            return Json(data);
+        }
+ 
+
+        //Add person
+        public IActionResult AddPerson([FromBody] PersonModel model)
+        {
+            if (!ModelState.IsValid) return UnprocessableEntity("Model state invalid");
+
+            var isPhone = _db.Person.IsPhoneExist(model.Phone);
+            if (isPhone) return UnprocessableEntity("Mobile number already exists");
+
+            var response = _db.Person.Add(model);
+
+            return Ok(response);
+        }
+        
+        //Get parson
+        public async Task<IActionResult> GetPerson(string name)
+        {
+            var response = await _db.Person.SearchAsync(name);
+            return Json(response);
+        }
+
+        //Delete parson
+        public IActionResult DeletePerson(int id)
+        {
+            var response = _db.Person.Delete(id);
+            return Json(response.IsSuccess);
+        }
+
+
+        //Add loan
+        public IActionResult AddLoan()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddLoan(PersonalLoanAddModel model)
+        {
+            model.RegistrationId = _db.Registrations.GetRegID_ByUserName(User.Identity.Name);
+            var response = _db.PersonalLoan.Add(model);
+            return Json(response);
+        }
+
+        //get loan
+        public IActionResult GetLoanDataTable(DataRequest request)
+        {
+            var data = _db.PersonalLoan.ListDataTable(request);
+            return Json(data);
+        }
+
+        //Delete loan
+        public IActionResult DeleteLoan(int id)
+        {
+            var response = _db.PersonalLoan.Delete(id);
+            return Json(response.IsSuccess);
+        }
+
+    }
+}
