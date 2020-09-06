f2c7f4a846906c0d5b561752421e3595bb16629b
muhebollah <muhebollah.diu@gamil.com>
2020-09-06 12:49:17 +0600

    change

--- AhmedTrading.Web/Controllers/SellingController.cs
+++ AhmedTrading.Web/Controllers/SellingController.cs
@@ -202,5 +202,12 @@ namespace AhmedTrading.Web.Controllers
             var data = _db.SellingPayments.ReceiptDataTable(request);
             return Json(data);
         }
+
+        //Delete Payment Summary
+        public IActionResult DeletePaymentSummary(int id)
+        {
+            var response = _db.SellingPayments(id);
+            return Json(response.IsSuccess);
+        }
     }
 }
